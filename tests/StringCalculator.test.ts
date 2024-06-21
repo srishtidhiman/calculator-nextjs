c75be22a034c0bb2840c050fcb0e71a1efb01a82
Srishti <srishti.dhiman1@gmail.com>
2024-06-21 15:50:28 +0530

    Implement single number addition

--- tests/StringCalculator.test.ts
+++ tests/StringCalculator.test.ts
@@ -10,4 +10,7 @@ describe('StringCalculator', () => {
   test('should return 0 for an empty string', () => {
     expect(calculator.add("")).toBe(0);
   });
+  test('should return the number for a single number string', () => {
+    expect(calculator.add("1")).toBe(1);
+  });
 });
